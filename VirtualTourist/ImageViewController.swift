2e2a03eb638718c03465d754d74b8231e1e55f07
Elliot T <tikhomirovi@icloud.com>
2016-01-30 14:04:12 -0500

    Implement views and pins

---
+++ VirtualTourist/ImageViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ImageViewController.swift
+//  VirtualTourist
+//
+//  Created by Ilia Tikhomirov on 30/01/16.
+//  Copyright © 2016 Ilia Tikhomirov. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ImageViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- VirtualTourist/ViewController.swift
+++ VirtualTourist/ViewController.swift
@@ -7,12 +7,43 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, UIGestureRecognizerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mapView.delegate = self
+        
+        //Long Press guesture recognizer
+        let uilgr = UILongPressGestureRecognizer(target: self, action: "addAnnotation:")
+        uilgr.minimumPressDuration = 0.5
+        uilgr.delegate = self
+        mapView.addGestureRecognizer(uilgr)
+        
+    }
+    
+    func addAnnotation(gestureRecognizer:UIGestureRecognizer) {
+        
+         if gestureRecognizer.state == UIGestureRecognizerState.Began {
+            
+            //Getting location from the map
+            let touchPoint = gestureRecognizer.locationInView(mapView)
+            let newCoordinates = mapView.convertPoint(touchPoint, toCoordinateFromView: mapView)
+            //Creating a generic pin
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = newCoordinates
+            //Add new annotation
+            mapView.addAnnotation(annotation)
+            
+            print(newCoordinates)
+            print("LongPress")
+            
+         }
     }
 
     override func didReceiveMemoryWarning() {
