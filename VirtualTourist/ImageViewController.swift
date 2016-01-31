dc83fa19149ae48e3f9261b95e605e5637f1d095
Elliot T <tikhomirovi@icloud.com>
2016-01-31 15:16:35 -0500

    Complete the user interface

--- VirtualTourist/ImageViewController.swift
+++ VirtualTourist/ImageViewController.swift
@@ -9,14 +9,28 @@
 import UIKit
 import MapKit
 
-class ImageViewController: UIViewController, MKMapViewDelegate {
+class ImageViewController: UIViewController, MKMapViewDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
 
+    @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var flowL: UICollectionViewFlowLayout!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+                
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = UIColor.whiteColor()
+        
+        let space: CGFloat = (view.frame.size.width / 64.0)
+        let dimension = (view.frame.size.width  / 3.2)
+        
+        flowL.minimumInteritemSpacing = space
+        flowL.minimumLineSpacing = space
+        flowL.sectionInset = UIEdgeInsets(top: space, left: space, bottom: space, right: space)
+        flowL.itemSize = CGSizeMake(dimension, dimension)
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,7 +38,36 @@ class ImageViewController: UIViewController, MKMapViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+       
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CollectionViewCell", forIndexPath:indexPath)
+        
+        cell.backgroundColor = UIColor.blueColor()
+        
+        return cell
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView, didHighlightItemAtIndexPath indexPath: NSIndexPath) {
+        
+        let cell = collectionView.cellForItemAtIndexPath(indexPath)
+        
+    cell!.backgroundColor = UIColor.blueColor().colorWithAlphaComponent(0.5)
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView, didUnhighlightItemAtIndexPath indexPath: NSIndexPath) {
+        let cell = collectionView.cellForItemAtIndexPath(indexPath)
+        cell!.backgroundColor = UIColor.blueColor()
+    }
+    
+    
+    
     /*
     // MARK: - Navigation
 
--- VirtualTourist/ViewController.swift
+++ VirtualTourist/ViewController.swift
@@ -40,15 +40,67 @@ class ViewController: UIViewController, MKMapViewDelegate, UIGestureRecognizerDe
             //Add new annotation
             mapView.addAnnotation(annotation)
             
+            
             print(newCoordinates)
             print("LongPress")
             
          }
     }
+    
+    func mapView(mapView: MKMapView, didAddAnnotationViews views: [MKAnnotationView]) {
+        print(__FUNCTION__)
+        
+        var i = -1;
+        for view in views {
+            i++;
+            if view.annotation is MKUserLocation {
+                continue;
+            }
+            
+            // Check if current annotation is inside visible map rect, else go to next one
+            let point:MKMapPoint  =  MKMapPointForCoordinate(view.annotation!.coordinate);
+            if (!MKMapRectContainsPoint(self.mapView.visibleMapRect, point)) {
+                continue;
+            }
+            
+            let endFrame:CGRect = view.frame;
+            
+            // Move annotation out of view
+            view.frame = CGRectMake(view.frame.origin.x, view.frame.origin.y - self.view.frame.size.height, view.frame.size.width, view.frame.size.height);
+            
+            // Animate drop
+            let delay = 0.03 * Double(i)
+            UIView.animateWithDuration(0.5, delay: delay, options: UIViewAnimationOptions.CurveEaseIn, animations:{() in
+                view.frame = endFrame
+                // Animate squash
+                }, completion:{(Bool) in
+                    UIView.animateWithDuration(0.05, delay: 0.0, options: UIViewAnimationOptions.CurveEaseInOut, animations:{() in
+                        view.transform = CGAffineTransformMakeScale(1.0, 0.6)
+                        
+                        }, completion: {(Bool) in
+                            UIView.animateWithDuration(0.3, delay: 0.0, options: UIViewAnimationOptions.CurveEaseInOut, animations:{() in
+                                view.transform = CGAffineTransformIdentity
+                                }, completion: nil)
+                    })
+                    
+            })
+        }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
+        print("Pressed")
+        performSegueWithIdentifier("images", sender: self)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let backItem = UIBarButtonItem()
+        backItem.title = "OK"
+        navigationItem.backBarButtonItem = backItem // This will show in the next view controller being pushed
     }
 
 
